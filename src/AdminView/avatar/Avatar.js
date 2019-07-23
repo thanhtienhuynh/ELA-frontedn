0ff633ac8bb001bcb47bcedc54c5dce63a5dc6ad
Mr Stark <tien01662892891@gmail.com>
2019-07-23 10:20:28 +0700

    fix format project

--- src/AdminView/avatar/Avatar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Image } from 'semantic-ui-react'
-
-const ImageLink = () => (
-    <Image  style={{ marginBottom: '3em' }} src='https://react.semantic-ui.com/images/wireframe/square-image.png' circular />
-)
-
-export default ImageLink
--- src/CommonComponent/Pagination.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Pagination } from 'semantic-ui-react'
-
-const PaginationE = () => <Pagination defaultActivePage={5} totalPages={10} />
-
-export default PaginationE;
